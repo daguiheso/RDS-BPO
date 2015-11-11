0195cf6399b3dac7f249defb8794f40aae0b8f79
daniel Ferrans <diferrans@gmail.com>
2015-11-11 15:21:12 -0500

    Directivas y models

--- assets/js/form.js
+++ assets/js/form.js
@@ -2,10 +2,20 @@
 angular.module('formulario', [])
     .controller('ExampleController', ['$scope', function($scope) {
       $scope.list = [];
+	  $scope.name = '';
+	  $scope.email = '';
+	  $scope.phone = ''; 
+	  $scope.pais = ''; 
       $scope.text = 'hello';
+	  
+	  
       $scope.submit = function() {
         if ($scope.text) {
           $scope.list.push(this.text);
+		   $scope.list.push(this.name);
+		    $scope.list.push(this.email);
+			$scope.list.push(this.phone);
+			$scope.list.push(this.pais);
           $scope.text = '';
         }
       };
