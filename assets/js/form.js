4fc51d070b58f725b537b3e3a7146fcba69defb3
Guillermo Hernandez <frontend@reddesignsystems.com>
2015-11-11 18:23:55 -0500

    http refactor

--- assets/js/form.js
+++ assets/js/form.js
@@ -6,33 +6,40 @@ angular.module('formulario', [])
 	  $scope.email = '';
 	  $scope.phone = ''; 
 	  $scope.pais = ''; 
-      $scope.text = 'hello';
+      // $scope.text = 'hello';
 	  
 	  
       $scope.submit = function() {
-        if ($scope.text) {
-          $scope.list.push(this.text);
-		   $scope.list.push(this.name);
+        // if ($scope.text) {
+            // $scope.list.push(this.text);
+		    $scope.list.push(this.name);
 		    $scope.list.push(this.email);
 			$scope.list.push(this.phone);
 			$scope.list.push(this.pais);
 			console.log(this.pais);
-          $scope.text = '';
-			
+            $scope.text = '';
+			debugger
 			//console.log(data);
 			//console.log("Antes de llamar submit");
-		  $http.post('/action.php', {nombre: this.name} ).then(function successCallback(response) {
-    // this callback will be called asynchronously
-    // when the response is available
-	console.log(response);
-  }, function errorCallback(response) {
-    // called asynchronously if an error occurs
-	console.log("error al enviar");
-    // or server returns response with an error status.
-  });
+			var req = {
+				method: 'POST',
+				url: 'http://landing.reddesignsystems.com/action.php',
+				data: $scope.list				
+			}
+			$http(req)
+				.then(function successCallback(response) {
+					debugger
+	    			// this callback will be called asynchronously
+	    			// when the response is available
+					console.log(response);
+ 				 }, function errorCallback(response) {
+    				// called asynchronously if an error occurs
+					console.log("error al enviar");
+    				// or server returns response with an error status.
+  				});
 		  
 		  
 		  
-        }
-      };
+        // }
+        };
     }]);
