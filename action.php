44fa3e538b96281083b5a4064ac0fb09ac2c8c37
daniel Ferrans <diferrans@gmail.com>
2015-11-12 10:26:22 -0500

    envio

--- action.php
+++ action.php
@@ -3,38 +3,16 @@
 require_once 'vendor/autoload.php';
 
 $contenido = json_decode(file_get_contents("php://input"));
-//echo $contenido->name;
-
-
-//var_dump($contenido);
-//die;
-
-//$nombre =  $_POST['nombre'];
-//$empresa = $_POST['empresa'];
-//$email = $_POST['email'];
-//$telefono = $_POST['telefono'];
-//$pais = $_POST['pais'];
 $ok = 0;
 $ip = $_SERVER['REMOTE_ADDR'];
-//mysql
-//$con=mysqli_connect("localhost","landing","landing","landing");
-// Check connection
-//if (mysqli_connect_errno()) {
-//  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-//}
-//
-
-//
-$ok = 1;
-$arr = array ('status'=>2,'nombre'=>$contenido->name, 'email' => $contenido->email, 'telefono' => $contenido->phone, 'pais' => $contenido->pais, 'ip' =>$ip );
-
-
+$arr = array ('status'=>0,'mensaje'=>'Por favor diligencie los campos e intente de nuevo');	
+if($contenido->name !="" || $contenido->email !="" || $contenido->phone !="" || $contenido->pais !="" ){
+$arr = array ('status'=>1,'mensaje'=>'Se envio el formulario con exito');
+//$arr = array ('status'=>2,'nombre'=>$contenido->name, 'email' => $contenido->email, 'telefono' => $contenido->phone, 'pais' => $contenido->pais, 'ip' =>$ip );	
+$ok = 1;	
+}
 // echo json con respuesta de valores enviados.
 if($ok == 1){
- //guardar en base de datos y enviar correo electronico
-//mysqli_query($con,"INSERT INTO landing (id, nombre, empresa, email, telefono, servicios, ip) VALUES (NULL, '$nombre', '$empresa', '$email', '$telefono', '$servicios', '$ip')");
-//mysqli_close($con);
-//
 $mail = new PHPMailer;
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mandrillapp.com';  // Specify main and backup SMTP servers
@@ -42,11 +20,10 @@ $mail->SMTPAuth = true;                               // Enable SMTP authenticat
 $mail->Username = 'soporte@reddesignsystems.com';                 // SMTP username
 $mail->Password = 'l2yC0meh1MNLLHegs2i-Ng';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
-
+$mail->Port = 587;                                   // TCP port to connect to
 $mail->From = 'soporte@reddesignsystems.com';
 $mail->FromName = 'Soporte landing';
-$mail->addAddress('flb@reddesignsystems.com', 'Maria paula');     // Add a recipient
+//$mail->addAddress('flb@reddesignsystems.com', 'Maria paula');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 $mail->addCC('soporte@reddesignsystems.com');
@@ -64,10 +41,8 @@ $message .= "<tr><td><strong>Email:</strong> </td><td>$contenido->email</td></tr
 $message .= "<tr><td><strong>Telefono:</strong> </td><td>$contenido->phone </td></tr>";
 $message .= "<tr><td><strong>Pais:</strong> </td><td>$contenido->pais </td></tr>";
 $message .= "<tr><td><strong>Dirip:</strong> </td><td>$ip </td></tr>";
-
 $message .= "</table>";
 $message .= "</body></html>";
-
 //
 $mail->Subject = 'Landing csd bpo';
 $mail->Body    = $message;
